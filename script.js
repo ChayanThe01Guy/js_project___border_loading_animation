5c109af50d1452ab1628f9f53422cfa46bbf305e
Chayan Roy <83724456+ChayanThe01Guy@users.noreply.github.com>
2022-11-11 19:58:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const container = document.querySelector(".container");
+const percent = document.querySelector("#percent");
+
+let perVal = 0;
+
+let increment = setInterval(() => {
+    perVal++;
+    percent.textContent = `${perVal}%`;
+    
+    if (perVal == 100) {
+        clearInterval(increment);
+        container.classList.remove("active");
+    }
+}, 100);
